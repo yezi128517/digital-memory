8ad56d38269fc6cb367a46ce07983e7258910f2f
yezi128517 <yezi128517@gmail.com>
2026-03-14 14:51:17 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // 1. 必须导入这个 v4 专用插件
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // 2. 必须在这里激活它，否则 CSS 里的 @import "tailwindcss" 无效
+    tailwindcss(),
   ],
-  base: '/digital-memory/', // 3. 确保你的部署路径正确
+  base: '/digital-memory/',
 })
